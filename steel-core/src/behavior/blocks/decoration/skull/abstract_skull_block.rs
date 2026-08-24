effc3dfaab4b571450df52bcfdeb2282440ea02a
RedCommander735 <40245965-RedCommander735@users.noreply.github.com>
2026-08-24 19:45:54 +0200

    feat: AbstractSkullBlock

--- steel-core/src/behavior/blocks/decoration/skull/abstract_skull_block.rs
+++ steel-core/src/behavior/blocks/decoration/skull/abstract_skull_block.rs
@@ -1,11 +1,16 @@
-use std::sync::Weak;
+use std::sync::{Arc, Weak};
 use steel_macros::block_behavior;
+use steel_registry::block_entity_type::BlockEntityTypeRef;
+use steel_registry::blocks::block_state_ext::BlockStateExt;
 use steel_registry::blocks::BlockRef;
 use steel_registry::blocks::properties::{BlockStateProperties, BoolProperty, EnumProperty, RailShape};
 use steel_utils::{BlockPos, BlockStateId};
+use steel_utils::types::UpdateFlags;
 use crate::behavior::{BlockBehavior, BlockEntityCreation, BlockPlaceContext};
 use crate::behavior::blocks::PoweredBlock;
-use crate::world::World;
+use crate::block_entity::BlockEntityTicker;
+use crate::entity::ai::path::PathComputationType;
+use crate::world::{SignalGetter, World};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SkullBlockType {
@@ -18,25 +23,41 @@ pub enum SkullBlockType {
     Dragon,
 }
 
+pub(super) const POWERED: &BoolProperty = &BlockStateProperties::POWERED;
+
 /// Shared server behavior inherited from vanilla's `AbstractSkullBlock`.
 pub(super) trait AbstractSkullBlock {
-    const POWERED: &BoolProperty = &BlockStateProperties::POWERED;
 
     /// Should be const
     #[must_use]
     fn new(block: BlockRef, skull_type: SkullBlockType) -> impl AbstractSkullBlock;
+    fn get_type() -> SkullBlockType;
 }
 
-pub(super) trait SharedSkullBlockBehavior: BlockBehavior {
-    fn skull_get_state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId>
+pub(super) trait AbstractSkullBlockBehavior: BlockBehavior {
+    #[must_use]
+    fn state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId>;
 }
 
-impl<T: SharedSkullBlockBehavior> BlockBehavior for T {
+impl<T: AbstractSkullBlockBehavior> BlockBehavior for T {
+    #[must_use]
     fn get_state_for_placement(&self, context: &BlockPlaceContext<'_>) -> Option<BlockStateId> {
-        self.skull_get_state_for_placement(context)
+        self.state_for_placement(context)
     }
+
+    fn is_pathfindable(&self, _state: BlockStateId, _computation_type: PathComputationType) -> bool {
+        false
+    }
+
     fn new_block_entity(&self, level: Weak<World>, pos: BlockPos, state: BlockStateId) -> BlockEntityCreation {
         // TODO: Implement SkullBlockEntity
         todo!("Implement SkullBlockEntity")
+    }
+
+    fn handle_neighbor_changed(&self, state: BlockStateId, world: &Arc<World>, pos: BlockPos, _source_block: BlockRef, _moved_by_piston: bool) {
+        let signal: bool = world.has_neighbor_signal(pos);
+        if signal != state.get_value(POWERED) {
+            world.set_block(pos, state.set_value(POWERED, signal), UpdateFlags::UPDATE_CLIENTS);
+        }
     }
 }
